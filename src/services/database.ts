94f4541c3d39a2a82e9d9f5302502802e860dd41
FPC-VIAUC <354412@via.dk>
2026-05-11 11:53:51 +0200

    Add support for multiple models
    Small refactor in there aswell...

---
+++ src/services/database.ts
@@ -0,0 +1,103 @@
+import { Client, Result } from "pg";
+import format from "pg-format";
+
+import { Weather, PredictionResponse } from "../schema";
+
+export let database = null;
+
+export default class Database {
+  config: Record<string, any> = {};
+  client: Client = null;
+  connected: boolean = false;
+
+  constructor(config) {
+    this.config = config;
+  }
+
+  async connect() {
+    try {
+      this.client = new Client(this.config);
+      await this.client.connect();
+      this.connected = true;
+      console.log("Database connected successfully.");
+      return this.client;
+    } catch (error) {
+      console.error("Error connecting to the database:", error);
+      this.connected = false;
+    }
+  }
+
+  async disconnect() {
+    try {
+      if (this.connected) {
+        await this.client.end();
+        this.connected = false;
+        console.log("Database disconnected successfully.");
+      }
+    } catch (error) {
+      console.error("Error disconnecting from the database:", error);
+    }
+  }
+
+  async savePredictions(response: PredictionResponse) {
+    const [model_name, versions] = response.model.split("-");
+    const [model_major_version, model_minor_version] = versions.split("_");
+
+    const values: Array<Array<any>> = response.predictions.map((p) => [
+      p.predicted_time,
+      p.prediction_offset,
+      p.temperature,
+      p.humidity,
+      Math.floor(p.wind_direction),
+      p.wind_speed,
+      p.precipitation,
+      Math.floor(p.light),
+      model_name,
+      model_major_version,
+      model_minor_version,
+    ]);
+
+    try {
+      const result = await this.client.query(
+        format(
+          'INSERT INTO "WeatherPrediction" (predicted_time, prediction_offset, temperature, humidity, wind_direction, wind_speed, precipitation, light, model_name, model_major_version, model_minor_version) VALUES %L',
+          values,
+        ),
+      );
+      return result.rows;
+    } catch (e) {
+      throw Error(`Could not save predictions to database: ${e}`);
+    }
+  }
+
+  async readLatestWeather(): Promise<Weather> {
+    try {
+      const result: Result = await this.client.query(
+        'SELECT * FROM "Weather" WHERE time = (SELECT MAX(time) from "Weather")',
+      );
+
+      if (result.rowCount == 0) throw Error("No weather in database");
+      return result.rows[0];
+    } catch (e) {
+      throw Error(`Could not read latest weather from database: ${e}`);
+    }
+  }
+
+  async readModelNames(): Promise<Array<string>> {
+    try {
+      const result: Result = await this.client.query(
+        'SELECT DISTINCT name FROM "Model"',
+      );
+
+      return result.rows.map((row) => row.name);
+    } catch (e) {
+      throw Error(`Could not read model names from database: ${e}`);
+    }
+  }
+}
+
+export const createDatabaseConnection = async (passwordConfig) => {
+  database = new Database(passwordConfig);
+  await database.connect();
+  return database;
+};
---
+++ src/services/predictions_service.ts
@@ -0,0 +1,24 @@
+import { PredictionRequest, PredictionResponse } from "../schema";
+
+import { API_DOMAIN, API_KEY } from "../config.js";
+
+export async function makePredictions(
+  req: PredictionRequest,
+): Promise<PredictionResponse> {
+  return fetch(`${API_DOMAIN}/v1/predictions`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "x-functions-key": API_KEY,
+    },
+    body: JSON.stringify(req),
+  })
+    .then(async (res) => {
+      if (!res.ok) throw Error(await res.text());
+      return res;
+    })
+    .then((res) => res.json())
+    .catch((e) => {
+      throw Error(`Error when making predictions: ${e}`);
+    });
+}
